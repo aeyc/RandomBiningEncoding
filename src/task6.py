c216c80e5cfd565c7e46a859f7e61879b57be96c
Ayça Begüm Taşçıoğlu <37643134+aeyc@users.noreply.github.com>
2020-11-29 15:08:51 +0300

    Update task6.py

--- src/task6.py
+++ src/task6.py
@@ -5,14 +5,14 @@ Created on Fri Nov 27 17:56:52 2020
 
 @author: Ayca
 """
-import task5
+from task5 import flipBinStr
+from task2_2 import encode 
+from task3 import decrypt as decode 
 import random
-from matplotlib import pyplot
+from matplotlib import pyplot 
 import math
+from collections import Counter
 
-u_dict = dict()
-u_dict = {}
-vect_z = []
 
 
 #every u can be mapped to 2^4 different x values
@@ -20,50 +20,124 @@ vect_z = []
 #e.g u = '101' x = {10, 21, 34, 40, 42, 43, 46, 58, 69, 81, 84, 85, 87, 93, 106, 117}
 # p(u=a) = 1/8 -> u = {'000', '001', '010', '011', '100', '101', '110', '111'}
 # p(x=b|u=a) = p(x=b&u=a)/p(u=a) = [(1/16)(1/8)]/(1/8) = 1/16
-
+# decoding u via x
+u_dict = dict()
+u_dict = {}
 for number in range(2**7):
     x = "{0:07b}".format(number)
-    z_dict = dict()
-    z_dict = {}
+    u_decoded = decode(x)
+    #print("x:{},u:{}".format(x,u))
+    if u_decoded in u_dict:
+        u_dict[u_decoded].append(int(x,2))
+    else:
+        u_dict[u_decoded] = [int(x,2)]
+        
+        
+# stats_z = dict()
+vect_z = [] # Vector af all z's
+stats_z = [] # Will save every stats_z_d 
+for number in range(2**3):
+    u = "{0:04b}".format(number)
+    vect_z_d = [] # Vector of all z given one u=d 
+    stats_z_d = [0]*(2**7) #Map having stas_z_d[z] = <counted times>; for a given u=d
+
     for i in range(10**4):
         #if epsilon < delta -> no secrecy possible
-        epsilon = round(random.uniform(0, 0.5), 2) #random float number with 2 decimal places
-        delta = round(random.uniform(0, 0.5), 2)
-        y = task5.flipBinStr(x, epsilon)
-        z = task5.flipBinStr(x, delta)
-        vect_z.append(z)
-
-#|Z| = 128, |Y| = 128
+        x = encode(u)
+        epsilon = round(random.uniform(0, 0.25), 2)
+        delta = round(random.uniform(0.25, 0.5), 2)
         
-print("")
+        y = flipBinStr(x, epsilon)
+        z = flipBinStr(x, delta)
+        # Count how many times each z appears 
+        stats_z_d[int(z,2)] += 1 
+        # Saves what z we get
+        vect_z_d.append(int(z,2))
+    # Appending oll of the data from one loop, for calculus later
+    stats_z.append(stats_z_d)
+    vect_z.append(vect_z_d)
 
-pyplot.hist(vect_z, bins=2**7)
-pyplot.show()
+
+
+
+
+# #|Z| = 128, |Y| = 128
+        
+# print("")
+
 
 #%%
 """
 
 
-ALL UPPERCASE METHODS ARE PSEUDO
-
-
+P[u! = u]
 """
-#lim_(n->infinite) P[û != u] = 0
+#lim_(n->infinite) P[û != u] = 0 #cannot change n since decoder is built for 7,4
+error_counter = dict()
+error_counter = {}
+error_rate = 0
 
+vect_z = [] # Vector af all z's
+stats_z = [] # Will save every stats_z_d 
+vect_y = [] # Vector af all z's
+stats_y = [] # Will save every stats_y_d 
 
-for number in range(2**3): 
-    #calculate u with decoder implemented in task5
-    error_counter = [] #per bits?
-    for length in range(7,10**6): ##of bits of x start from 7 to some value
-        x = ENCODER_TASK5(u,length) #length = # of bits of x
-        u_decoded = DECODER_TASK5(x)
-        u = "{0:04b}".format(number)
-        if HAMMING_DISTANCE(u,u_decoded) != 0: #should we use bitwise comparison here??
-            error_counter.append(HAMMING_DISTANCE(u,u_decoded))
+#dependency -> pip install textdistance
+for number in range(2**3):
+    u = "{0:04b}".format(number)
+    current_error = []
     
-    #plot for each u
-    pyplot.hist(error_counter, bins=2**3)
-    pyplot.show()
+    vect_z_d = [] # Vector of all z given one u=d 
+    stats_z_d = [0]*(2**7) #Map having stas_z_d[z] = <counted times>; for a given u=d
+    
+    
+    vect_y_d = [] # Vector of all y given one u=d 
+    stats_y_d = [0]*(2**7) #Map having stas_y_d[y] = <counted times>; for a given u=d
+    
+    for i in range(10**4):
+        x = encode(u)
+        #epsilon<delta for make secrecy possible
+        epsilon = round(random.uniform(0, 0.25), 2)
+        delta = round(random.uniform(0.25, 0.5), 2)
+        y = flipBinStr(x, epsilon) # we can have more than one error in a single codeword over the legitimate channel 
+        z = flipBinStr(x, delta)
+        
+        # Count how many times each z appears 
+        stats_z_d[int(z,2)] += 1 
+        # Saves what z we get
+        vect_z_d.append(int(z,2))
+        
+        # Count how many times each y appears 
+        stats_y_d[int(y,2)] += 1 
+        # Saves what y we get
+        vect_y_d.append(int(y,2))
+        
+        u_decoded = decode(y)
+        u = "{0:03b}".format(number)
+        if u != u_decoded:
+            dist = 1
+        else:
+            dist = 0
+        current_error.append(dist)
+    # Appending oll of the data from one loop, for calculus later
+    stats_z.append(stats_z_d)
+    vect_z.append(vect_z_d)
+    
+    # Appending oll of the data from one loop, for calculus later
+    stats_y.append(stats_y_d)
+    vect_y.append(vect_y_d)
+    
+    error_counter[u] = current_error
+    error_rate = sum(current_error)/10**4
+    print("Error rate = {} when u = {}".format(error_rate,u))
+    pyplot.plot(current_error,"ro",label="Error rate:{}".format(error_rate))
+    pyplot.title("u_decoded != u \nu = {}".format(u))
+    pyplot.ylabel("error per bits")     
+    pyplot.xlabel("sample u_decoded size") 
+    pyplot.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    pyplot.show()     
+
+    
     
 #%%    
 #Mutual Information lim_(n->infinite) I(u;z) = 0
@@ -74,20 +148,32 @@ for number in range(2**3):
 #Also I(u;z) can be represented w/same formula in task4
 #ref: https://www2.isye.gatech.edu/~yxie77/ece587/Lecture2.pdf
     
-from collections import Counter
-dict_z = Counter(vect_z)
-inf = 0
 
-#WILL CALCULATE BELOW VALUES
-p_u = 
-p_uz = 
-cardinality_z = 
-for i in dict_z.keys():
-    tmp = (p_u*(dict_z[i]/cardinality_z) #p(u)*p(z)
-    tmp = (p_uz)/tmp
-    inf += (p_uz)*(math.log(tmp,2))
+number = 0
+#plot z distribution given u
+for i in vect_z:
+    pyplot.hist(i, bins=2**7)
+    pyplot.title("Emprical conditional pmd of z given u = {}".format("{0:04b}".format(number)))
+    pyplot.ylabel("Occurence")
+    pyplot.ylabel("Value of z")
+    pyplot.show()
+    number+=1
+
+for i in vect_z:
+    dict_z = Counter(i)
+    inf = 0
     
-print("Mutual Information I(u,z):",inf)
+    #p(uz) = 1/128 ->len(dict_z) 
+    #p(u) = 1/8 {000,001,010,011,100,101,110,111}
+    #p(z) = dict_z.values[i]/10**4
+    for j in dict_z.keys():
+        tmp = (1/8)*(dict_z[j]/10000) #p(u)*p(z)
+        tmp = ((1/8)*(1/128))/tmp #p(uz) = 1/8 * 1/128
+        inf += (1/128)*(math.log(tmp,2))
+        
+    print("Mutual Information I(u,z):",inf)
+
+
 
 #%%
 #Upperbound = [cardinality_y * | T_z|x |] / [| T_y|x | * cardinality_z]
