a9a7f7abbd4776849916f22f61797d3b0e43747d
Ayça Begüm Taşçıoğlu <37643134+aeyc@users.noreply.github.com>
2020-11-29 22:37:54 +0300

    added if __name__ == "__main__":    -takes to much time in task6

--- src/task5.py
+++ src/task5.py
@@ -31,41 +31,41 @@ def randBinStr(length):
     for i in range(length): 
         bin_str += str(random.randint(0,1))
     return bin_str
+if __name__ == "__main__":    
+    # params of binary symmetric channel 
+    epsilon = 0.2 
+    delta = 0.4
     
-# params of binary symmetric channel 
-epsilon = 0.2 
-delta = 0.4
-
-# long binary sequence 
-x = randBinStr(10 ** 6)
-
-y = flipBinStr(x, epsilon)
-z = flipBinStr(x, delta) 
-
-# print("X: %s\nY: %s\nZ: %s" % (x, y, z))
-
-## estimate the value of epsilon and delta, looking at the nuber of flipped bits in y and z  
-y_flippedBits = sum(val != y[index] for index, val in enumerate(x))
-estimated_epsilon = float(y_flippedBits) / len(x)
-print("Epsilon: %f - Estimated epsilon: %f" % (epsilon, estimated_epsilon))
-
-z_flippedBits = sum(val != z[index] for index, val in enumerate(x))
-estimated_delta = float(z_flippedBits) / len(x)
-print("Delta: %f - Estimated delta: %f" % (delta, estimated_delta))
-
-## connect the wiretap channel to the random binning encoder - decoder, and simulate several trasmissions 
-for i in range(10 ** 8):
-    x = randBinStr(3) 
+    # long binary sequence 
+    x = randBinStr(10 ** 6)
     
-    # encoder 
-    x_encoded = encode(x) 
+    y = flipBinStr(x, epsilon)
+    z = flipBinStr(x, delta) 
     
-    # wiretap channel 
-    y = flipBinStr(x_encoded, epsilon) # we can have more than one error in a single codeword over the legitimate channel 
-    z = flipBinStr(x_encoded, delta) 
+    # print("X: %s\nY: %s\nZ: %s" % (x, y, z))
     
-    # decoder 
-    y_decoded = decode(y) 
-    z_decoded = decode(z) 
+    ## estimate the value of epsilon and delta, looking at the nuber of flipped bits in y and z  
+    y_flippedBits = sum(val != y[index] for index, val in enumerate(x))
+    estimated_epsilon = float(y_flippedBits) / len(x)
+    print("Epsilon: %f - Estimated epsilon: %f" % (epsilon, estimated_epsilon))
+    
+    z_flippedBits = sum(val != z[index] for index, val in enumerate(x))
+    estimated_delta = float(z_flippedBits) / len(x)
+    print("Delta: %f - Estimated delta: %f" % (delta, estimated_delta))
+    
+    ## connect the wiretap channel to the random binning encoder - decoder, and simulate several trasmissions 
+    for i in range(10 ** 8):
+        x = randBinStr(3) 
         
-    # print("X: %s, Encoded X: %s, Y: %s, Decoded Y: %s, Z: %s, Decoded Z: %s" % (x, x_encoded, y, y_decoded, z, z_decoded))
+        # encoder 
+        x_encoded = encode(x) 
+        
+        # wiretap channel 
+        y = flipBinStr(x_encoded, epsilon) # we can have more than one error in a single codeword over the legitimate channel 
+        z = flipBinStr(x_encoded, delta) 
+        
+        # decoder 
+        y_decoded = decode(y) 
+        z_decoded = decode(z) 
+            
+        # print("X: %s, Encoded X: %s, Y: %s, Decoded Y: %s, Z: %s, Decoded Z: %s" % (x, x_encoded, y, y_decoded, z, z_decoded))
